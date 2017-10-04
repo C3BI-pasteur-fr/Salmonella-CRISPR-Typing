24a002ea101ebbbab5c366906dc2db1802688fc5
khillion <hillion.kenzo@gmail.com>
2017-10-04 16:28:48 +0200

    update tests

--- test/unit_test.py
+++ test/unit_test.py
@@ -13,6 +13,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
 from salmonella_crispr import main
+from salmonella_crispr.settings import LOCAL_DATA
 
 
 class TestMain(unittest.TestCase):
@@ -33,10 +34,10 @@ class TestMain(unittest.TestCase):
         Test for finding 
         """
         res = main.find_spacers(self.query[0], self.spacers)
-        self.assertEqual(str(res.seq), "aa-spacer1tcccggg")
+        self.assertEqual(str(res.seq), "aa-spacer1&tcccggg")
         self.assertEqual(res.id, "query1")
         res = main.find_spacers(self.query[1], self.spacers)
-        self.assertEqual(str(res.seq), "aa-spacer2tcccggg")
+        self.assertEqual(str(res.seq), "aa-spacer2&tcccggg")
         self.assertEqual(res.id, "query2")
 
     def test_uniq_write_fasta(self):
@@ -64,6 +65,27 @@ class TestMain(unittest.TestCase):
             res.append(main.find_spacers(query, self.spacers))
         with open (tmp_file, 'w') as file_handle:
             main.write_fasta(res, file_handle)
+        try:
+            self.assertTrue(filecmp.cmp(expected_file, tmp_file))
+        finally:
+            os.remove(tmp_file)
+
+    def test_truncation(self):
+        """
+        End to end test for truncation of long genomic regions
+        """
+        tmp_file = 'tmp_test_trunc.output'
+        input_file = os.path.dirname(__file__) + '/long_sequence.fa'
+        expected_file = os.path.dirname(__file__) + '/trunc_example.test.output'
+        # Open files
+        with open(input_file, 'r') as file_handle:
+            query = list(SeqIO.parse(file_handle, "fasta"))
+        with open(LOCAL_DATA + "/spacers_Salmonella.fa", "r") as file_handle:
+            spacers = list(SeqIO.parse(file_handle, "fasta"))
+        res_query = [main.find_spacers(query[0], spacers)]
+        res_query = main.truncate_sequences(res_query)
+        with open (tmp_file, 'w') as file_handle:
+            main.write_fasta(res_query, file_handle)
         try:
             self.assertTrue(filecmp.cmp(expected_file, tmp_file))
         finally:
